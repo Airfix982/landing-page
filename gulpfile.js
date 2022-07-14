446bbf923ca4e6cbfd848e836eaee672f59c6edb
Кадников Николай <25roter52@gmail.com>
2022-07-14 17:40:21 +0400

    now exactly like on the site

--- gulpfile.js
+++ gulpfile.js
@@ -34,8 +34,6 @@ var gulp = require("gulp"),
 };
 
     var browserSync = require("browser-sync").create();
-
-
     
     function style() {
 	
@@ -57,41 +55,15 @@ var gulp = require("gulp"),
         
                 .pipe(gulp.dest(paths.styles.dest))
         
-                // Add browsersync stream pipe after compilation
-        
                 .pipe(browserSync.stream())
         
         );
         
     }
-    
-   /* function style() 
-    {
-        return (
-            gulp
-                .src("styles/*.sass")
-                .pipe(sourcemaps.init())    // Initialize sourcemaps before compilation starts
-                .pipe(sass())      
-                .on("error", sass.logError)
-                .pipe(postcss([autoprefixer(), cssnano()]))// Use postcss with autoprefixer and compress the compiled file using cssnano
-                .pipe(sourcemaps.write())  // Now add/write the sourcemaps     
-                .pipe(gulp.dest("styles"))   
-                .pipe(browserSync.stream()) 
-                .pipe(dest('app/css'))
-
-        );       
-    }*/
 
     exports.style = style;
 
-/*function styles()
-{
-    return  src('app/scss/style.scss')
-        .pipe(sass())
-        .pipe(dest('app/css'))
-}
 
-exports.styles = styles;*/
 
 function reload() {
 	
@@ -114,32 +86,9 @@ function watch()
 	
     gulp.watch(paths.styles.src, style);
 	
-    // We should tell gulp which files to watch to trigger the reload
-	
-    // This can be html or whatever you're using to develop your website
-	
-    // Note -- you can obviously add the path to the Paths object
-	
     gulp.watch("/home/cossieman2000/WORK/landing-page/*.html", reload);
 	
 }
-
-
-/*function watch()
-{
-    browserSync.init({
-	
-        server: {
-	
-            baseDir: "./"
-	
-        }
-	
-    });
-	
-}*/
-
-//gulp.watch(paths.styles.src, style);
 
 gulp.watch('app/sass/*.sass', style);//old
 
